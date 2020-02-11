674a0a3b81280598bc992090618aa759cffd01f1
Kimball Springall <ks@churchofjesuschrist.org>
2020-02-11 14:16:53 -0700

    Added a controller

---
+++ src/main/java/com/pipelinetest/pipetest/controller/MainController.java
@@ -0,0 +1,48 @@
+package com.pipelinetest.pipetest.controller;
+
+import com.pipelinetest.pipetest.Greeting;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class MainController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class.getName());
+
+    @RequestMapping("/")
+    public String greeting() {
+        return "Hello World";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/g")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/tester")
+    public String greetingTest() {
+        return "Hello Tester!";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/kimball")
+    public String greetingKimball() {
+        return "Hello Kimball!";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/n")
+    public Greeting greetingName(@RequestParam(value = "name", defaultValue = "World") String name) {
+        LOGGER.info("The /n endpoint was called");
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/greeting/{personName}")
+    public Greeting greetingPost(@PathVariable("personName") String personName) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, personName));
+    }
+
+}
