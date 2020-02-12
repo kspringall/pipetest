a83ea63ed1c650b1fb1c0c3d74b1c992dd4dbb68
Kimball Springall <ks@churchofjesuschrist.org>
2020-02-12 13:30:15 -0700

    updated example test

--- tests/example_test.py
+++ tests/example_test.py
@@ -1,10 +1,4 @@
-import os
 import requests
-
-for root, dirs, files in os.walk("."):
-    for filename in files:
-        print(filename)
-
 
 r = requests.get('http://localhost:8080/kimball')
 print(r)
